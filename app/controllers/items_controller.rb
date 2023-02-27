c4a73aece359d51db15c238669ebf7418cb1e00d
Nick Scofield <c.nick.scofield@gmail.com>
2023-02-27 14:43:18 -0700

    commit tree 47875cf42f0c3d12bcc9406443a0003805772215

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,32 @@
 class ItemsController < ApplicationController
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
   def index
-    items = Item.all
-    render json: items, include: :user
+    if (params[:user_id])
+      items = User.find(params[:user_id]).items
+    else
+      items = Item.all
+    end
+
+    render json: items, include: :user, status: :ok
   end
 
+  def show
+    render json: Item.find(params[:id]), status: :ok
+  end
+
+  def create
+    user = User.find(params[:user_id])
+    item = user.items.create(item_params)
+    render json: item, include: :user, status: :created
+  end
+
+  private
+  def item_params
+    params.permit(:name, :description, :price, :user_id)
+  end
+
+  def render_not_found_response(exception)
+    render json: { error: "#{exception.model} not found" }, status: :not_found
+  end
 end
